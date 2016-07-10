1b6a07f85d6efea430c3d9b2c308d8b0ef96c04a
osmar106 <osmar106@gmail.com>
2016-07-09 21:16:23 -0400

    Code working with basic animations

--- Filtered/ViewController.swift
+++ Filtered/ViewController.swift
@@ -10,9 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var filteredImage: UIImage?
+    
+    @IBOutlet var imageView: UIImageView!
+    
+    @IBOutlet var filterButton: UIButton!
+    
+    @IBOutlet var secondaryMenu: UIView!
+    @IBOutlet var bottomMenu: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        secondaryMenu.backgroundColor = UIColor.whiteColor().colorWithAlphaComponent(0.5)
+        secondaryMenu.translatesAutoresizingMaskIntoConstraints = false
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,5 +34,44 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func onFilter(sender: UIButton) {
+        if(sender.selected){
+            hideSecondaryMenu()
+            sender.selected = false
+        }
+        else {
+            showSecondaryMenu()
+            sender.selected = true
+        }
+
+    }
+    
+    func showSecondaryMenu() {
+        view.addSubview(secondaryMenu)
+        
+        let bottomConstraint = secondaryMenu.bottomAnchor.constraintEqualToAnchor(bottomMenu.topAnchor)
+        let leftConstraint = secondaryMenu.leftAnchor.constraintEqualToAnchor(view.leftAnchor)
+        let rightConstraint = secondaryMenu.rightAnchor.constraintEqualToAnchor(view.rightAnchor)
+        let heightConstraint = secondaryMenu.heightAnchor.constraintGreaterThanOrEqualToConstant(44)
+        
+        NSLayoutConstraint.activateConstraints([bottomConstraint, leftConstraint, rightConstraint, heightConstraint])
+        
+        view.layoutIfNeeded()
+        
+        self.secondaryMenu.alpha = 0
+        UIView.animateWithDuration(0.4) {
+            self.secondaryMenu.alpha = 1.0
+        }
+    }
+    
+    func hideSecondaryMenu() {
+        UIView.animateWithDuration(0.4, animations: { 
+            self.secondaryMenu.alpha = 0.4
+            }) { (completed) in
+                if completed == true {
+                    self.secondaryMenu.removeFromSuperview()
+                }
+        }
+    }
 }
 
